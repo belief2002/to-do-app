995f8703a793a5ed786b79486011c98cf3828706
belief2002 <satyampatel30smart@gmail.com>
2024-02-27 18:13:19 +0530

    few changes

--- app/todos/page.js
+++ app/todos/page.js
@@ -140,9 +140,9 @@ const Todos = () => {
     );
   }
   return (
-    <div className="flex flex-col items-center pt-8 gap-8">
+    <div className=" h-full flex flex-col items-center pt-8 gap-8 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="text-2xl font-semibold">Todos</div>
-      <div className="flex gap-2 py-2">
+      <div className="flex gap-2 py-2 flex-col items-center md:flex-row">
         <input
           className="text-xl rounded-lg border-none px-1 shadow"
           type="text"
@@ -151,20 +151,23 @@ const Todos = () => {
           onChange={(e) => setInputText(e.target.value)}
           onKeyUp={(e) => {addTodoKey(e);}}
         />
+      <div className=" gap-2 flex">
+
         <button
           className="bg-blue-500 rounded-md py-1 px-2 text-white hover:bg-blue-600"
           onClick={addTodo}
-        >
+          >
           Add
         </button>
         <button
           onClick={clearTodo}
           className="bg-gray-500 rounded-md py-1 px-2 text-white hover:bg-gray-600"
-        >
+          >
           Clear All
         </button>
+          </div>
       </div>
-      <div className="flex gap-3 flex-col  w-1/2">
+      <div className="flex gap-3 flex-col  w-1/2 max-md:hidden">
         {todos.map((todos) => (
           <div
             key={todos["id"]}
@@ -193,12 +196,51 @@ const Todos = () => {
                   deleteTodo(todos["id"]);
                 }}
               >
-                Delete{" "}
+                Delete
               </button>
             </div>
           </div>
         ))}
       </div>
+
+      {/* mobile devices */}
+      <div className="flex gap-3 flex-col  md:hidden max-md:flex max-md:flex-col">
+  {todos.map((todo) => (
+    <div
+      key={todo.id}
+      className="flex flex-col  pl-2 rounded-lg justify-between  items-center "
+    >
+      <div className="flex bg-slate-300 rounded-md pl-2 gap-2">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => {}}
+        />
+        <div className="bg-slate-300 w-auto px-5 rounded-md">{todo.desc}</div>
+      </div>
+      <div className="flex items-center justify-center gap-2 mt-2 max-md:mt-0">
+        <button
+          className="bg-green-500  rounded-md px-1 mt-2 text-white hover:bg-green-600"
+          onClick={() => {
+            editTodo(todo);
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="bg-red-500 rounded-md  mt-2  px-2 text-white hover:bg-red-600"
+          onClick={() => {
+            deleteTodo(todo.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  ))}
+</div>
+
+
     </div>
   );
 };
--- components/Nav.js
+++ components/Nav.js
@@ -6,10 +6,10 @@ import { options } from "@/app/api/auth/[...nextauth]/options";
 const Nav = async () => {
   const session = await getServerSession(options);
   return (
-    <header className="bg-gray-600 text-gray-100">
+    <header className="bg-blue-800 text-gray-100">
       <nav className="flex justify-between items-center w-full px-10 py-4">
         <div>To Do</div>
-        <div className="flex gap-10">
+        <div className="flex gap-10 hover:outline rounded-md px-2 items-center justify-center outline-1 hover:bg-blue-900">
           {session ? (
             <Link href={"/api/auth/signout?callbackUrl=/"}>Logout</Link>
           ) : (
